3b53ef7a7cf75cfb47c91765afb7c74c8a84e1f1
mhisham11 <mmhashimmp10@gmail.com>
2026-07-21 20:43:28 +0530

    Finished Stopwatch.py

--- Manual/stopwatch.py
+++ Manual/stopwatch.py
@@ -1,2 +1,17 @@
 #python stopwatch
 import time
+
+startstopwatch = input("Press Enter to start the stopwatch")
+
+stopwatch = 0
+while True:
+    print("\033[H\033[2J", end="") #clears the screen
+
+    stopwatch += 1
+
+    seconds = stopwatch % 60            
+    minutes = (stopwatch//60) % 60
+    hours = (stopwatch//3600) 
+
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
